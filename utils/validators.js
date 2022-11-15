365f8726f2238a51abfba0b4dc791c48316c28e0
Dmitry Konychev <dalexeych@gmail.com>
2022-11-16 02:09:10 +0300

    fix: all backwards

--- utils/validators.js
+++ utils/validators.js
@@ -34,7 +34,7 @@ module.exports.postMovieValidator = celebrate({
 
 module.exports.deleteMoviesValidator = celebrate({
   params: Joi.object().keys({
-    _id: Joi.string(),
+    _id: Joi.string().length(24).hex(),
   }),
 });
 
